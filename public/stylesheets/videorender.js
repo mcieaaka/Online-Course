0375408e2ed76a20b9608d48d096fd7a57b69763
Harshit <harshit0300@gmail.com>
2021-05-16 01:30:40 +0530

    ui

--- public/stylesheets/videorender.js
+++ public/stylesheets/videorender.js
@@ -8,3 +8,19 @@ function render(s){
         }
     }
 }
+
+function finder(){
+    var e =new RegExp(document.getElementById("search-bar").value,"gi") ;
+    var ob = document.getElementsByClassName("course");
+    var c=0;
+    for(i=0;i<ob.length;i++){
+        if(e.test(ob[i].id)){
+            ob[i].style.display ="initial";
+            c++;
+        }
+        else{
+            ob[i].style.display ="none";
+        }
+    }
+    document.getElementById("results").innerHTML="Result(s) found:("+c+")";
+}
